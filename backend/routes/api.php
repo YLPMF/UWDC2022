6ad93cf39b03b01ce3390d29509b734cbfe52091
Aleksis Steinburgs <aleksis.steinburgs@gmail.com>
2022-01-08 15:54:51 +0200

    Fix typo in routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login'])->name('login');
+Route::post('login', [\App\Http\Controllers\API\AuthController::class, 'login'])->name('login');
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [\App\Http\Controllers\API\AuthController::class, 'me'])->name('me');
